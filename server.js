af2d6e1d7095169de7236dab75696eb2cbf56aff
João Fonseca <joaofranciscovilarinho@gmail.com>
2017-04-26 23:53:31 +0100

    Updated Files

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ var app = express();
 app.use(express.static(__dirname + '/html/files'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + "/html/home.html");
+	res.sendFile(__dirname + "/html/index.html");
+})
+
+app.get('/forum.html', function(req, res){
+	res.sendFile(__dirname + "/html/forum.html");
+})
+
+app.get('/games.html', function(req, res){
+	res.sendFile(__dirname + "/html/games.html");
+})
+
+app.get('/sign.html', function(req, res){
+	res.sendFile(__dirname + "/html/sign.html");
 })
 
 var server = app.listen(8081, function(){
