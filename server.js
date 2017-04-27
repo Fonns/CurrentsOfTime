2b0f5146d36ecacdf9b3b78bbf139f43b9808a33
João Fonseca <joaofranciscovilarinho@gmail.com>
2017-04-27 02:15:53 +0100

    Ready for presentation #2

--- server.js
+++ server.js
@@ -5,22 +5,26 @@ app.use(express.static(__dirname + '/html/files'));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + "/html/index.html");
+	console.log('\nAccess to home page.');
 })
 
-app.get('/forum.html', function(req, res){
+app.get('/forum', function(req, res){
 	res.sendFile(__dirname + "/html/forum.html");
+	console.log('\nAccess to forum.');
 })
 
-app.get('/games.html', function(req, res){
+app.get('/games', function(req, res){
 	res.sendFile(__dirname + "/html/games.html");
+	console.log('\nAccess to mini games.');
 })
 
-app.get('/sign.html', function(req, res){
+app.get('/login', function(req, res){
 	res.sendFile(__dirname + "/html/sign.html");
+	console.log('\nAccess to sign in.');
 })
 
 var server = app.listen(8081, function(){
 	var host = server.address().address;
 	var port = server.address().port
-	console.log('example app listening at http://%s:%s', host, port)
+	console.log('\nServer up and running.');
 })
